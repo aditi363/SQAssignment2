eae5cd4596e67a976749bf8e0dc4e4592617520d
Aditi Saini <sainiaditi67@gmail.com>
2023-03-29 10:20:25 -0400

    rectangle class as per assignment requirements

--- SQassignment2/Program.cs
+++ SQassignment2/Program.cs
@@ -1,2 +1,86 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        var returnBack = true;
+        do
+        {
+            int choice, r, l, w, b, h;
+            double area = 0;
+            double length = 0;
+            double width = 0;
+            double permimeter = 0;
+            int len = 0;
+            int wid = 0; 
+            
+
+            Console.Write("\n\n");
+            Console.Write("Select one of the following option in the menu:\n");
+            Console.Write("-----------------------------------------------");
+            Console.Write("\n\n");
+
+
+            Console.Write("1. Enter lenght of the rectangle\n");
+            Console.Write("2. Edit lenght of the rectangle \n");
+            Console.Write("3. Enter width of the rectangle\n");
+            Console.Write("4. Edit width of the rectangle\n ");
+            Console.Write("5. Calculate Area of rectangle\n ");
+            Console.Write("6. Calculate Perimeter of rectangle\n ");
+            Console.Write("7. Exit\n ");
+            Console.Write("-----------------------------------------------\n");
+
+
+            choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Input length  of the rectangle : ");
+                    len = Convert.ToInt32(Console.ReadLine());
+                    length = len;
+                    //Console.Write("The length entered is : \n", length);
+                    break;
+                case 2:
+                    Console.Write("Add new length  of the rectangle : ");
+                    len = Convert.ToInt32(Console.ReadLine());
+                    //Console.Write("The length entered is : \n", len);
+                    break;
+                case 3:
+                    Console.Write("Input width  of the rectangle : ");
+                    wid = Convert.ToInt32(Console.ReadLine());
+                    //Console.Write("The width entered is : \n", wid);
+                    break;
+                case 4:
+                    Console.Write("Add new width  of the rectangle : ");
+                    wid = Convert.ToInt32(Console.ReadLine());
+                    //Console.Write("The new width entered is : \n", wid);
+                    break;
+                case 5:
+                    Console.Write("Input length  of the rectangle : \n");
+                    l = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Input  width of the rectangle : ");
+                    w = Convert.ToInt32(Console.ReadLine());
+                    area = l * w;
+                    Console.Write("The area is : {0}\n", area);
+
+                    break;
+                case 6:
+                    Console.Write("Input length  of the rectangle : \n");
+                    l = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Input  width of the rectangle : ");
+                    w = Convert.ToInt32(Console.ReadLine());
+                    permimeter = (l + w) / 2;
+                    Console.Write("The permimeter is : {0}\n", permimeter);
+                   break;
+                case 7:
+                    return;
+                default:
+                    break;
+            }
+
+
+
+        } while (returnBack);
+    }
+}
--- SQassignment2/Rectangle.cs
+++ SQassignment2/Rectangle.cs
@@ -6,23 +6,89 @@ using System.Threading.Tasks;
 
 namespace SQassignment2
 {
-    internal class Rectangle
-    {
-        int lenght, widit;
+	public class Rectangle
+	{
+		private int length;
+		private int width;
 
-        //Default constructor 
-        public Rectangle()
-        {
-            lenght = 1;
-            widit = 1;
-        }
+		// default constructor
+		public Rectangle()
+		{
+			length = 1;
+			width = 1;
+		}
 
-        //Main method
-        public static void main()
-        {
-            Rectangle r = new Rectangle();
+		// non-default constructor
+		public Rectangle(int length, int width)
+		{
+			if (length < 1 || width < 1)
+			{
+				throw new ArgumentException("Invalid rectangle dimensions.");
+			}
 
-            Console.WriteLine("The default : "+ (r.lenght * r.widit));
-        }
-    }
+			this.length = length;
+			this.width = width;
+		}
+
+		public int GetCurrentLength()
+		{
+			return length;
+		}
+
+		public int SetNewLength(int length)
+		{
+			if (length < 1)
+			{
+				throw new ArgumentException("Invalid length.");
+			}
+
+			this.length = length;
+			return length;
+		}
+
+		public int GetCurrentWidth()
+		{
+			return width;
+		}
+
+		public int SetNewWidth(int width)
+		{
+			if (width < 1)
+			{
+				throw new ArgumentException("Invalid width.");
+			}
+
+			this.width = width;
+			return width;
+		}
+
+		public int GetPerimeter()
+		{
+			return 2 * (length + width);
+		}
+
+		public int GetArea()
+		{
+			return length * width;
+		}
+
+
+
+		public static void DisplayResults(int length, int width, int area, int perimeter)
+		{
+
+			Console.WriteLine("'Dimensions' of rectangle:-\n");
+			Console.WriteLine("{0, -15} {1,5:f1}", "Length of Rectangle is:", length);
+			Console.WriteLine("{0, -15} {1,5:f1}", "Width of Rectangle is:", width);
+			Console.WriteLine("{0, -15} {1,5:f1}", "Area of Rectangle is:", area);
+			Console.WriteLine("{0, -15} {1,5:f1}", "Perimeter of Rectangle is:", perimeter);
+			Console.WriteLine("\n");
+			Console.WriteLine("Press Any Key To 'Exit'");
+		}
+
+
+	}
+
+
+
 }
